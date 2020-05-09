f17ff1669c327f6e03bb03e1e403db795ef1e56e
Danya Rose <Danya.Scout@gmail.com>
2020-05-09 13:04:48 -0400

    Added building support for Windows and LInux

--- electron/main.js
+++ electron/main.js
@@ -1,3 +1,12 @@
+const setupEvents = require('./installers/setupEvents')
+ if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+ }
+
+const {ipcMain} = require('electron')
+var path = require('path')
+
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
